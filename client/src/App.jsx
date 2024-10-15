7bb2862acec1cd471c83e4a9dab558337f306872
monkCommits <speak2monk@gmail.com>
2024-10-15 17:34:04 +0530

    code editor done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,33 @@
 import { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 import "./App.css";
-
+import Editor from "@monaco-editor/react";
 const socket = io("http://localhost:5000");
 
 const App = () => {
   const [joined, setJoined] = useState(false);
   const [roomId, setRoomId] = useState("");
   const [userName, setUserName] = useState("");
+  const [language, setLanguage] = useState("javascript");
+  const [code, setCode] = useState("");
+  const [copySuccess, setCopySuccess] = useState("");
 
   const joinRoom = () => {
     if (roomId && userName) {
       socket.emit("join", { roomId, userName });
       setJoined(true);
     }
+  };
+  const copyRoomId = () => {
+    navigator.clipboard.writeText(roomId);
+    setCopySuccess("Copied");
+    setTimeout(() => {
+      setCopySuccess("");
+    }, 2000);
+  };
+
+  const handleCodeChange = (newCode) => {
+    setCode(newCode);
   };
 
   if (!joined) {
@@ -42,7 +56,50 @@ const App = () => {
       </div>
     );
   }
-  return <div>User joined</div>;
+  return (
+    <div className="editor-container">
+      <div className="sidebar">
+        <div className="room-info">
+          <h2>Code Room: {roomId}</h2>
+          <button onClick={copyRoomId} className="copy-button">
+            Copy Id
+          </button>
+          {copySuccess && <span className="copy-success">Copy Success</span>}
+        </div>
+        <h3>Users in Room :</h3>
+        <ul>
+          <li>kt</li>
+          <li>ss</li>
+        </ul>
+        <p className="typing-indicator">user typing...</p>
+        <select
+          className="language-selector"
+          value={language}
+          onChange={(e) => setLanguage(e.target.value)}
+        >
+          <option value="javascript">JavaScript</option>
+          <option value="python">Python</option>
+          <option value="Java">Java</option>
+          <option value="C++">C++</option>
+        </select>
+        <button className="leave-button">Leave Room</button>
+      </div>
+      <div className="editor-wrapper">
+        <Editor
+          height={"100%"}
+          defaultLanguage={language}
+          language={language}
+          value={code}
+          onChange={handleCodeChange}
+          theme="vs-dark"
+          options={{
+            minimap: { enabled: false },
+            fontSize: 14,
+          }}
+        ></Editor>
+      </div>
+    </div>
+  );
 };
 
 export default App;
