d6338a759459281aeb64968b4082de181154bfd3
monkCommits <speak2monk@gmail.com>
2024-10-23 18:35:49 +0530

    url corrected

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,7 +12,7 @@ import JoinRoom from "./Components/JoinRoom.jsx";
 import Sidebar from "./Components/Sidebar.jsx";
 import Output from "./Components/Output.jsx";
 
-const socket = io("https://devsync-LCOAL.onrender.com/");
+const socket = io("https://devsync-m54y.onrender.com/");
 
 export default function App() {
   const [joined, setJoined] = useState(false);
